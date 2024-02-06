0fd8c7fa0944226190b4a4e9c309349a9a9b624f
Aditya Agrahari <aditya07agrahari@gmail.com>
2024-02-06 11:32:42 +0530

    Adding Files

---
+++ src/models/product.js
@@ -0,0 +1,8 @@
+class product {
+    constructor(name, category, startPrice, sellerName){
+        this.name = name;
+        this.category = category;
+        this.startPrice = startPrice;
+        this.sellerName = sellerName;
+    }
+}
